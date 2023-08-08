d8bbf0dc4436c815d9c1c7cfea76d73094cb6824
Ervin <evgnz24@gmail.com>
2023-08-08 00:09:25 -0500

    Fixed image not loading, page now shows 'site is loading img'

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import './styles/App.css';
+import img from '../public/220880-P1KV8M-746.jpg';
 
 function App() {
   return (
     <>
       <div className='under-construction-img-container'>
-        <img
-          src='../src/assets/220880-P1KV8M-746.jpg'
-          alt='Page under construction'
-        ></img>
-      </div>
-      <div className='img-credits'>
-        Image by{' '}
-        <a href='https://www.freepik.com/free-vector/flat-construction-template_1584487.htm#query=website%20under%20construction&position=1&from_view=keyword&track=ais'>
-          Freepik
-        </a>
+        <img src={img} alt='Page under construction'></img>
+        <div className='img-credits'>
+          Image by
+          <a href='https://www.freepik.com/free-vector/flat-construction-template_1584487.htm#query=website%20under%20construction&position=1&from_view=keyword&track=ais'>
+            Freepik
+          </a>
+        </div>
       </div>
     </>
   );
